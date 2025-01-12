9eedb1716a2e6b38b529b2f16724ae4664584307
Admin32R2 <ralphtajantajan4@gmail.com>
2025-01-12 20:42:19 +0800

    backend_side

--- RND/settings.py
+++ RND/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'app_name',
+    'myapp',
+     'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'RND.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -66,6 +69,7 @@ TEMPLATES = [
         },
     },
 ]
+
 
 WSGI_APPLICATION = 'RND.wsgi.application'
 
--- RND/urls.py
+++ RND/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include  # Include for app urls
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('myapp.urls')),  # Include app's urls
+    path('', include('app_name.urls')),
 ]
+
+
